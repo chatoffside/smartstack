50a5c641895840c4e888e78c8f87f3df50d18a4f
RNDflo <rndfloinc@gmail.com>
2018-09-22 01:00:46 -0400

    Added LoginForm

--- smartstack-react/src/components/pages/LoginPage.js
+++ smartstack-react/src/components/pages/LoginPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import LoginForm from '../forms/LoginForm';
 
 const LoginPage = () => (
     <div>
@@ -8,29 +9,7 @@ const LoginPage = () => (
         &nbsp;
         <Link to="/login">Login</Link>
         <div className="main-container">
-            <h1>Bid for the property you wish to invest in and earn ROI in Ether</h1>
-            <div className="block">
-               <h4>Timer:</h4> &nbsp;
-               <span> this.state.timer</span>
-            </div>
-            <div className="block">
-               <h4>Last winner:</h4> &nbsp;
-               <span>this.state.lastWinner</span>
-            </div>
-            <hr/>
-            <h2>Vote for the next number</h2>
-            <ul>
-               <li onClick={() => {this.voteNumber(1)}}>1</li>
-               <li onClick={() => {this.voteNumber(2)}}>2</li>
-               <li onClick={() => {this.voteNumber(3)}}>3</li>
-               <li onClick={() => {this.voteNumber(4)}}>4</li>
-               <li onClick={() => {this.voteNumber(5)}}>5</li>
-               <li onClick={() => {this.voteNumber(6)}}>6</li>
-               <li onClick={() => {this.voteNumber(7)}}>7</li>
-               <li onClick={() => {this.voteNumber(8)}}>8</li>
-               <li onClick={() => {this.voteNumber(9)}}>9</li>
-               <li onClick={() => {this.voteNumber(10)}}>10</li>
-            </ul>
+            <LoginForm/>
         </div>
     </div>
 );
